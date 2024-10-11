fe3781979ba17897dc757817dc21cdac26e05963
Myhau96 <tranthihaudn96@gmail.com>
2024-10-11 20:14:07 +0700

    start project

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png'
+import imageProduct2 from './assets/images/2.png'
+import imageProduct3 from './assets/images/3.png'
+import imageProduct4 from './assets/images/4.png'
+import imageProduct5 from './assets/images/5.png'
+import imageProduct6 from './assets/images/6.png'
+import imageProduct7 from './assets/images/7.png'
+import imageProduct8 from './assets/images/8.png'
+export const products = [
+    {
+        id: 1,
+        name: 'Ghế gỗ màu nâu đệm xám',
+        price: 100,
+        image: imageProduct1,
+        description: 'Bộ bàn ghế đó được làm bằng gỗ xoan. Những người thợ khéo tay đã đóng lại và làm thành bộ bàn ghế đẹp và vô cùng hấp dẫn.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Ghế gỗ đệm xanh thấp',
+        price: 150,
+        image: imageProduct2,
+        description: 'Bộ bàn ghế được tạo thành từ những miếng ghép khác nhau. Hình ảnh của bộ bàn ghế đã tạo nên những ấn tượng sâu sắc cho người mới gặp lần đầu.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Ghế đen đệm gỗ siêu sang',
+        price: 180,
+        image: imageProduct3,
+        description: 'Chiếc ghế được làm chắc chắn và được sơn màu đen, một gam màu của nó toát lên sự sang trọng.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'Ghế ngồi thấp màu xanh bọc bốn phía',
+        price: 200,
+        image: imageProduct4,
+        description: 'Đường nét của chiếc bàn này được trang trí theo hướng thủ công hiện đại, mỗi đường nét đều vẽ ra như phượng múa rồng bay. Tất cả thể hiện sự trang nhã và sự gần gũi.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Ghế ngồi thấp màu nâu chắc chắn',
+        price: 250,
+        image: imageProduct5,
+        description: 'Bề mặt của bộ bàn ghế phẳng được trang trí và mài nhẵn ở giữa có hoa văn và các đường nét công phu.Trên bàn có những đường nét uyển chuyển và được làm rất cẩn thận, nó có bốn chân rất vững chắc và tạo nên những khoảng không gian riêng và vô cùng gần gũi cho con người.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Ghế gỗ cao đệm đen sang trọng',
+        price: 160,
+        image: imageProduct6,
+        description: ' Ghế thì được làm chắc chắn có tựa lưng và cũng được làm rất cẩn thận với tông màu vàng đã tạo nên một không khí trang nhã và nó có sức ảnh hưởng mạnh mẽ đến con người.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Ghế ngồi thấp màu nâu gần gũi',
+        price: 120,
+        image: imageProduct7,
+        description: 'Hai chiếc ghế đơn thì vuông vắn hơn nó không dài không hình chữ nhật nhưng cũng có thể dành cho hai người ngồi vào đó. Thật sự rất đẹp, thêm đó là một chiếc ghế nhỏ nữa.',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Ghế gỗ thấp đệm đen có tay cầm',
+        price: 220,
+        image: imageProduct8,
+        description: 'Bước vào phòng khách, ai cũng trầm trồ khen bộ bàn ghế sofa nhà em đẹp. Có lẽ, nó đẹp nhờ màu sắc rực rỡ và thiết kế độc đáo.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+];
